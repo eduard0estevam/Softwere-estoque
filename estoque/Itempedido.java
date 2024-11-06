cce36d0738b509a8d904acc34a8b6663dd8fb16f
eduard0estevam <e.estevamcarvalho@outlook.com>
2024-11-06 10:55:31 -0300

    .

--- estoque/Itempedido.java
+++ estoque/Itempedido.java
@@ -2,4 +2,13 @@ public class Itempedido {
     Produto produto;
     int quantidade ;
     String nome ;
+
+    public Itempedido(Produto produto , int quantidade, String nome){
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.nome = nome;
+    }
+
+    Itempedido item = new Itempedido( produto, 0 , null);
+    
 }
--- estoque/Pedido.java
+++ estoque/Pedido.java
@@ -2,15 +2,23 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Pedido {
-    Itempedido itemPedido ;
-     ArrayList itempedido = new ArrayList<Pedido>(); 
 
-    public Pedido(){
+    ArrayList itempedido = new ArrayList<Pedido>(); 
 
-
+     public void additempedido(Itempedido item){
+        this.itempedido.add(item);
     }
+ 
+     public void retiraritempedido(int idproduto){
+        
+        for( int x = 0; x < itempedido.size(); x++){
 
+            if (itempedido.get(x).produto.idproduto == idproduto ) {
+                this.itempedido.remove(x);
+            }
 
+            }
 
+     }
 
     }
