ca26ff7b973661b038246c8fae08e47356218179
eduard0estevam <e.estevamcarvalho@outlook.com>
2024-11-06 08:39:56 -0300

    a

--- estoque/Pedido.java
+++ estoque/Pedido.java
@@ -1,6 +1,16 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Pedido {
     Itempedido itemPedido ;
-    List itempedido; 
-}
+     ArrayList itempedido = new ArrayList<Pedido>(); 
+
+    public Pedido(){
+
+
+    }
+
+
+
+
+    }
